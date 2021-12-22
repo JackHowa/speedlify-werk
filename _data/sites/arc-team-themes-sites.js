b61f9bc43dcd716df1479ecb00c6821e2bae36b5
Jack Howard <jack.howard@washpost.com>
2021-12-22 15:07:41 -0600

    feat: Add themes cc sites

---
+++ _data/sites/arc-team-themes-sites.js
@@ -0,0 +1,12 @@
+module.exports = {
+	description: "Pre-Built Themes Sites",
+	options: {
+		frequency: 60 * 11 + 30, // 11h, 30m
+		freshChrome: "site",
+	},
+	urls: [
+		"https://corecomponents-the-gazette-prod.cdn.arcpublishing.com/2019/09/17/global-kitchen-sink-article/",
+		"https://corecomponents-the-gazette-prod.cdn.arcpublishing.com/all-blocks/",
+		"https://corecomponents-the-gazette-prod.cdn.arcpublishing.com"
+	]
+};
