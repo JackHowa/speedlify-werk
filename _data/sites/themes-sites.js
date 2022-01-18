5e67e799c0aee9332a565daaede5cc617f2d6089
Jack Howard <jackhowa@fastmail.com>
2022-01-18 13:59:17 -0600

    Comment out client sites for now

--- _data/sites/themes-sites.js
+++ _data/sites/themes-sites.js
@@ -5,15 +5,15 @@ module.exports = {
 		freshChrome: "site",
 	},
 	urls: [
-		"https://www.dagen.se/nyheter/2020/01/02/har-ar-de-tio-mest-lasta-dagense-artiklarna-under-2019/",
-		"https://www.dagen.se/",
-		"https://www.969theeagle.com/news/national/genesis-will-tour-again/ffDvWrbR5QI7afDl3JUOIJ/",
-		"https://www.dayton.com/events/here-where-find-local-handmade-treasures/mxZkLMpkQmp3Me0qmzN6mJ/",
-		"https://www.dayton.com",
-		"https://www.washingtonpost.com/politics/how-two-undocumented-housekeepers-took-on-the-president--and-revealed-trumps-long-term-reliance-on-illegal-immigrants/2019/12/04/3dff5b5c-0a15-11ea-bd9d-c628fd48b3a0_story.html",
-		"https://www.wsbtv.com/",
-		"https://www.wsbtv.com/news/local/gov-kemp-rivian-announce-5-billion-electric-vehicle-plant-georgia/NAWAXRYM2VBMBM52QLFRYCHLFQ/",
-		"https://www.thenationalnews.com/us/",
-		"https://www.thenationalnews.com/coronavirus/2021/12/01/omicron-variant-vaccine-test-cases-travel/"
+// 		"https://www.dagen.se/nyheter/2020/01/02/har-ar-de-tio-mest-lasta-dagense-artiklarna-under-2019/",
+// 		"https://www.dagen.se/",
+// 		"https://www.969theeagle.com/news/national/genesis-will-tour-again/ffDvWrbR5QI7afDl3JUOIJ/",
+// 		"https://www.dayton.com/events/here-where-find-local-handmade-treasures/mxZkLMpkQmp3Me0qmzN6mJ/",
+// 		"https://www.dayton.com",
+// 		"https://www.washingtonpost.com/politics/how-two-undocumented-housekeepers-took-on-the-president--and-revealed-trumps-long-term-reliance-on-illegal-immigrants/2019/12/04/3dff5b5c-0a15-11ea-bd9d-c628fd48b3a0_story.html",
+// 		"https://www.wsbtv.com/",
+// 		"https://www.wsbtv.com/news/local/gov-kemp-rivian-announce-5-billion-electric-vehicle-plant-georgia/NAWAXRYM2VBMBM52QLFRYCHLFQ/",
+// 		"https://www.thenationalnews.com/us/",
+// 		"https://www.thenationalnews.com/coronavirus/2021/12/01/omicron-variant-vaccine-test-cases-travel/"
 	]
 };
