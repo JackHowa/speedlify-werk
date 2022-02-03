866e2968803d5ec6d3f5de16ed9171b8d2e09a8c
Jack Howard <jackhowa@fastmail.com>
2022-02-03 11:03:07 -0600

    Add empty page with fusion

--- _data/sites/arc-team-themes-sites.js
+++ _data/sites/arc-team-themes-sites.js
@@ -7,6 +7,7 @@ module.exports = {
 	urls: [
 		"https://corecomponents-the-gazette-prod.cdn.arcpublishing.com/2019/09/17/global-kitchen-sink-article/",
 		"https://corecomponents-the-gazette-prod.cdn.arcpublishing.com/all-blocks/",
-		"https://corecomponents-the-gazette-prod.cdn.arcpublishing.com"
+		"https://corecomponents-the-gazette-prod.cdn.arcpublishing.com",
+		"https://corecomponents-the-gazette-prod.cdn.arcpublishing.com/empty/"
 	]
 };
