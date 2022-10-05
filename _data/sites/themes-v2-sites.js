7be496303fda150fc7d4af06469f6f69d7055415
Jack Howard <jackhowa@fastmail.com>
2022-10-05 14:49:16 +0000

    chore: add cdn page

---
+++ _data/sites/themes-v2-sites.js
@@ -0,0 +1,10 @@
+module.exports = {
+	description: "v2 Themes Sites",
+	options: {
+		frequency: 60 * 11 + 30, // 11h, 30m
+		freshChrome: "site",
+	},
+	urls: [
+		"https://themesinternal-themesinternal-dev.web.arc-cdn.net/",
+	]
+};
